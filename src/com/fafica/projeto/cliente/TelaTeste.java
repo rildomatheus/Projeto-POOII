7d1e727bbcce2d8d189a136b46fb784ade685609
Lynneker Ramos <Lynneker Ramos@Dexter>
2015-11-12 00:37:30 -0300

    testa ai rildo

--- src/com/fafica/projeto/cliente/TelaTeste.java
+++ src/com/fafica/projeto/cliente/TelaTeste.java
@@ -1,10 +1,12 @@
 package com.fafica.projeto.cliente;
 
+import com.fafica.projeto.fachada.Fachada;
+
 public class TelaTeste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Fachada fachada = Fachada.getInstance();
 	}
 
 }
