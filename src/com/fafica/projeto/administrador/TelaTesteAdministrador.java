f720e9cfac69507df6dcbd2c1b355e425cf1cbd1
RMatheus <RMatheus@RMatheus-PC>
2015-11-18 06:09:14 -0200

    TEla

--- src/com/fafica/projeto/administrador/TelaTesteAdministrador.java
+++ src/com/fafica/projeto/administrador/TelaTesteAdministrador.java
@@ -1,11 +1,12 @@
 package com.fafica.projeto.administrador;
 
 import com.fafica.projeto.fachada.Fachada;
+import com.fafica.projeto.funcionario.CampoObrigatorioInvalidoException;
 
 public class TelaTesteAdministrador {
 	public static void main(String[] args){
 		
-		Administrador adm = new Administrador(3456,"Rildo Matheus");
+		Administrador adm = new Administrador(ai,"Rildo Matheus");
 		Administrador adm1 = new Administrador(3457,"Rildo Matheus");
 		Administrador adm2 = new Administrador(3458,"Sr. Rildo Matheus");
 		Fachada fachada = Fachada.getInstance();
@@ -17,7 +18,18 @@ public class TelaTesteAdministrador {
 		///fachada.removerAdministrador(3458);
 		//System.out.println(fachada.listarAdministrador());
 		
-		
+		try {
+			fachada.cadastrarAdministrador(adm);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		} catch (CampoObrigatorioInvalidoException e) {
+			System.out.println(e.getMessage());
+		} catch (AdministradorJaCadastradoException e) {
+			System.out.println(e.getMessage());
+		}
+		catch (Exception e){
+			System.out.println("ERROR!");
+		}
 	}
 
 }
