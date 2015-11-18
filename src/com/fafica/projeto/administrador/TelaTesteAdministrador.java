bfa2ec73892cf4fc57e720e01250ec3f3e34af24
RMatheus <RMatheus@RMatheus-PC>
2015-11-18 06:31:56 -0200

    TelaAdm

--- src/com/fafica/projeto/administrador/TelaTesteAdministrador.java
+++ src/com/fafica/projeto/administrador/TelaTesteAdministrador.java
@@ -6,7 +6,7 @@ import com.fafica.projeto.funcionario.CampoObrigatorioInvalidoException;
 public class TelaTesteAdministrador {
 	public static void main(String[] args){
 		
-		Administrador adm = new Administrador(ai,"Rildo Matheus");
+		Administrador adm = new Administrador(12,"Rildo Matheus");
 		Administrador adm1 = new Administrador(3457,"Rildo Matheus");
 		Administrador adm2 = new Administrador(3458,"Sr. Rildo Matheus");
 		Fachada fachada = Fachada.getInstance();
@@ -30,6 +30,42 @@ public class TelaTesteAdministrador {
 		catch (Exception e){
 			System.out.println("ERROR!");
 		}
+		
+		try {
+			fachada.atualizarAdministrador(adm);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());;
+		} catch (AdministradorNaoEncontradoException e) {
+			System.out.println(e.getMessage());
+		} catch (com.fafica.projeto.administrador.CampoObrigatorioInvalidoException e) {
+			System.out.println(e.getMessage());
+		}catch (Exception e){
+			System.out.println("ERROR Critical!!");
+		}
+		
+		try {
+			fachada.removerAdministrador(12);
+		} catch (AdministradorNaoEncontradoException e) {
+			System.out.println(e.getMessage());
+		} catch (com.fafica.projeto.administrador.CampoObrigatorioInvalidoException e) {
+			System.out.println(e.getMessage());
+		}catch (Exception e){
+			System.out.println("ERROR Desconhecido!!");
+		}
+		
+		try {
+			fachada.procurarAdministrador(12);
+		} catch (AdministradorNaoEncontradoException e) {
+			System.out.println(e.getMessage());
+		} catch (com.fafica.projeto.administrador.CampoObrigatorioInvalidoException e) {
+			System.out.println(e.getMessage());
+		}catch (Exception e){
+			System.out.println("ERROR");
+		}
+		
+		System.out.println(fachada.listarAdministrador());
 	}
 
+	    
+	    
 }
